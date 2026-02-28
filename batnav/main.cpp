574391f46d53aefcecb4beb6ab82199401120c9a
boh-pixel <alberto.calistri09@gmail.com>
2026-02-28 12:00:48 +0100

    base per analisi funzionale

--- batnav/main.cpp
+++ batnav/main.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+/*
+    analisi tecnica : funzioni usate
+
+    menu
+        gestione navi
+        inizio gioco
+        classifica
+        esci
+
+    gestione navi
+        numero navi predefinito      5 navi
+        dimensioni predefinite
+        vuoi modificare
+        per orgni nave scegliere le dimensioni
+
+    controlloPosizionamentoNavi
+
+
+
+*/
+
 int main()
 {
-    cout << "Alberto Calistri" << endl;
+    cout << "..." << endl;
+    int nNaviPred = 3, lungN1Predef = 2, lungN2Predef = 3, lungN3Predef = 4;
+    cout << "Il numero di navi predefiniuto e' : " << nNaviPred << endl;
+    cout << "Vuoi modificare il numero delle navi, la loro lunghezza o entrambe le cose ? " << endl;
+    cout << "1. Numero navi" << endl << "2. Lunghezza navi " << endl << "3. Entrambe" << endl << "4. Vanno bene le impostazioni predefinite" << endl;
+
+    return 0;
 }
