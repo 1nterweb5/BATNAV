6a27d0651ed953962f3e4938449a5bb2c93f6c63
unknown <tophatlucas@gmail.com>
2026-04-03 19:39:37 +0200

    un po di robina

--- batnav/main.cpp
+++ batnav/main.cpp
@@ -11,7 +11,6 @@
 #define GIU  80
 #define SX 75
 
-
 using namespace std;
 
 const int MAIN_BG = 9;
@@ -22,91 +21,85 @@ const int ERROR_FG = 4;
 
 struct Giocatore {
     char nome[15];
-    char scacchiera[30][10] = {'O'};
+    char scacchieraDifesa[10][30];
+    char scacchieraAttacco[10][30];
     int vittorie;
+    int colpiASegno; // Serve per contare quando si vince
 };
 
-void menu(int naviDa6, int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi);
-void scelta(int naviDa6, int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi);
-void iniziaGioco(int naviDa6 ,int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi, Giocatore ordinePlayer);
-void gestioneNavi(int nNavi6, int nNavi5, int nNavi4, int nNavi3, int nNavi2, int sommaNavi);
+// Struct per la classifica senza usare i file
+struct Record {
+    char nome[15];
+    int flotta;
+    int tempoImpiegato; // Calcolato in secondi base
+};
+
+Record classificaTop[10]; // Vettore globale per tenere i primi 10
+int numGiocatoriInClassifica = 0;
+bool giocoInEsecuzione = true; // Per gestire l'uscita
+
+// Aggiunte le '&' ai parametri per salvare le modifiche fatte nei menu
+void menu(int &naviDa6, int &naviDa5, int &naviDa4, int &naviDa3, int &naviDa2, int &totNavi);
+void scelta(int &naviDa6, int &naviDa5, int &naviDa4, int &naviDa3, int &naviDa2, int &totNavi);
+void iniziaGioco(int naviDa6 ,int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi);
+void gestioneNavi(int &nNavi6, int &nNavi5, int &nNavi4, int &nNavi3, int &nNavi2, int &sommaNavi);
 void classifica();
 void esci();
-void posizionamentoNavi(int nave);
+void posizionamentoNavi(int nave, Giocatore &p);
 void stampoNavi(int nave, int rotazione, int x, int y);
+void scacchiere();
+void faseBattaglia(Giocatore &p1, Giocatore &p2, int flottaTotale);
 
-void menu (int naviDa6, int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi) {
-    cout << "	      BATTAGLIA NAVALE                Data : ";
+void menu (int &naviDa6, int &naviDa5, int &naviDa4, int &naviDa3, int &naviDa2, int &totNavi) {
+    clearScreen(MAIN_BG, MAIN_FG);
+    setColor(MAIN_BG, MAIN_FG);
+    cout << "          BATTAGLIA NAVALE                Data : ";
     displayToday();
     cout << endl << "===============================================================" << endl;
-    gotoXY(12,5);
-    cout << "Selezione opzione :";
-    gotoXY(14,7);
-    cout << "1. Inizia Gioco";
-    gotoXY(14,9);
-    cout << "2. Gestione Navi";
-    gotoXY(14,11);
-    cout << "3. Classifica";
-    gotoXY(14,13);
-    cout << "4. Esci";
-    gotoXY(0,17);
-    cout << "===============================================================" << endl;
+    gotoXY(12,5); cout << "Selezione opzione :";
+    gotoXY(14,7); cout << "1. Inizia Gioco";
+    gotoXY(14,9); cout << "2. Gestione Navi";
+    gotoXY(14,11); cout << "3. Classifica";
+    gotoXY(14,13); cout << "4. Esci";
+    gotoXY(0,17); cout << "===============================================================" << endl;
     scelta(naviDa6, naviDa5, naviDa4, naviDa3, naviDa2, totNavi);
 }
 
-void scelta(int naviDa6, int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi) {
+void scelta(int &naviDa6, int &naviDa5, int &naviDa4, int &naviDa3, int &naviDa2, int &totNavi) {
     int selezione, newPos = 7, oldPos = 7;
-    Giocatore player1, player2;
-
     char comando;
     do {
-        gotoXY(12,oldPos);
-        cout << " ";
-        gotoXY(12,newPos);
-        cout << ">";
+        gotoXY(12,oldPos); cout << " ";
+        gotoXY(12,newPos); cout << ">";
         oldPos = newPos;
         comando = _getch();
-        if(comando == SU && newPos != 7) {
-            newPos -= 2;
-        }else if(comando == SU && newPos == 7) {
-            newPos = 13;
-        }
-        if(comando == GIU && newPos != 13) {
-            newPos += 2;
-        }else if(comando == GIU && newPos == 13){
-            newPos = 7;
-        }
+        if(comando == SU && newPos != 7) newPos -= 2;
+        else if(comando == SU && newPos == 7) newPos = 13;
+
+        if(comando == GIU && newPos != 13) newPos += 2;
+        else if(comando == GIU && newPos == 13) newPos = 7;
+
         if (comando == ENTER) {
              selezione = newPos;
              switch (selezione) {
                   case 7 :
-                      clearScreen(MAIN_BG, MAIN_FG);
-                      setColor(MAIN_BG, MAIN_FG);
-                      iniziaGioco(naviDa6, naviDa5, naviDa4, naviDa3, naviDa2, totNavi, player1);
+                      iniziaGioco(naviDa6, naviDa5, naviDa4, naviDa3, naviDa2, totNavi);
                       break;
                   case 9 :
-                      clearScreen(MAIN_BG, MAIN_FG);
-                      setColor(MAIN_BG, MAIN_FG);
                       gestioneNavi(naviDa6, naviDa5, naviDa4, naviDa3, naviDa2, totNavi);
                       break;
                   case 11 :
-                      clearScreen(MAIN_BG, MAIN_FG);
-                      setColor(MAIN_BG, MAIN_FG);
                       classifica();
                       break;
                   case 13 :
                       esci();
                       break;
-              }
+             }
         }
-    }while (comando != ENTER && comando != ESC);
+    } while (comando != ENTER && comando != ESC);
 }
 
 void scacchiere () {
-    cout << "	      BATTAGLIA NAVALE                Data : ";
-    displayToday();
-    gotoXY(0,1);
-    cout << "===============================================================" << endl;
     gotoXY(0,2);
     cout << R"(
     0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3
@@ -123,196 +116,181 @@ H   O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
 I   O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
 J   O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
 )";
-    gotoXY(0,17);
-    cout << "===============================================================" << endl;
-
+    gotoXY(0,17); cout << "===============================================================" << endl;
 }
 
 void stampoNavi (int nave, int rotazione, int x, int y) {
     if (rotazione == 0) {
         for (int i = 0; i < nave; i++) {
-            gotoXY(x, y);
-            cout << 'O';
-            x++;
-            cout << ' ';
-            x++;
+            gotoXY(x, y); cout << 'X';
+            x += 2;
         }
-    }else if (rotazione == 1) {
+    } else if (rotazione == 1) {
         for (int i = 0; i < nave; i++) {
-            gotoXY(x, y);
-            cout << 'O';
+            gotoXY(x, y); cout << 'X';
             y++;
         }
     }
-
-
 }
 
-void posizionamentoNavi (int nave) {
+// Passiamo il giocatore in modo da salvare la nave nella sua matrice
+void posizionamentoNavi (int nave, Giocatore &p) {
     char comando;
     int rotazione = 0;
     int x = 4, y = 6;
-    setColor(INFO_FG, MAIN_FG);
+    bool posizionata = false;
 
+    setColor(INFO_FG, MAIN_FG);
     stampoNavi(nave, rotazione, x, y);
 
     do {
         comando = _getch();
-        if (comando == SX) {
-            if (x != 4) {
-                setColor(MAIN_BG, MAIN_FG);
-                stampoNavi(nave, rotazione, x, y);
-                x-=2;
-                setColor(INFO_FG, MAIN_FG);
-                stampoNavi(nave, rotazione, x, y);
-            }
-        }
-        if (comando == DX) {
-            if (rotazione == 0) {
-                if (x != 52) {
-                    setColor(MAIN_BG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                    x+=2;
-                    setColor(INFO_FG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                }
-            }else if (rotazione == 1) {
-                if (x != 62) {
-                    setColor(MAIN_BG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                    x+=2;
-                    setColor(INFO_FG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                }
-            }
-        }
-        if (comando == GIU) {
-            if (rotazione == 0) {
-                if (y != 15) {
-                    setColor(MAIN_BG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                    y++;
-                    setColor(INFO_FG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                }
-            }else if (rotazione == 1) {
-                if (y != 10) {
-                    setColor(MAIN_BG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                    y++;
-                    setColor(INFO_FG, MAIN_FG);
-                    stampoNavi(nave, rotazione, x, y);
-                }
-            }
+        // Cancella la vecchia posizione ristampandola coi colori base (qui si dovrebbe usare la matrice, ma lo teniamo semplice come l'hai fatto tu)
+        setColor(MAIN_BG, MAIN_FG);
+        stampoNavi(nave, rotazione, x, y);
 
+        if (comando == SX && x != 4) x -= 2;
+        if (comando == DX) {
+            if (rotazione == 0 && x != 4 + (30 - nave)*2) x += 2;
+            else if (rotazione == 1 && x != 62) x += 2;
         }
-        if (comando == SU) {
-            if (y != 6) {
-                setColor(MAIN_BG, MAIN_FG);
-                stampoNavi(nave, rotazione, x, y);
-                y--;
-                setColor(INFO_FG, MAIN_FG);
-                stampoNavi(nave, rotazione, x, y);
-            }
+        if (comando == SU && y != 6) y--;
+        if (comando == GIU) {
+            if (rotazione == 0 && y != 15) y++;
+            else if (rotazione == 1 && y != 6 + (10 - nave)) y++;
         }
         if (comando == SPACE) {
-            if (rotazione == 0 && y < 11) {
-              setColor(MAIN_BG, MAIN_FG);
-              stampoNavi(nave, rotazione, x, y);
-              rotazione++;
-              setColor(INFO_FG, MAIN_FG);
-              stampoNavi(nave, rotazione, x, y);
-
-            }else if (rotazione == 1 && x < 54) {
-              setColor(MAIN_BG, MAIN_FG);
-              stampoNavi(nave, rotazione, x, y);
-              rotazione--;
-              setColor(INFO_FG, MAIN_FG);
-              stampoNavi(nave, rotazione, x, y);
-            }
+            if (rotazione == 0 && y <= 15 - nave + 1) rotazione = 1;
+            else if (rotazione == 1 && x <= 62 - nave*2 + 2) rotazione = 0;
         }
 
-    }while(nave != 0);
+        setColor(INFO_FG, MAIN_FG);
+        stampoNavi(nave, rotazione, x, y);
 
+        if (comando == ENTER) {
+            // Calcolo indici matrice (approssimativo in base alle tue coordinate video)
+            int col = (x - 4) / 2;
+            int riga = y - 6;
+
+            // Salviamo nella matrice del giocatore
+            for(int i = 0; i < nave; i++) {
+                if(rotazione == 0) p.scacchieraDifesa[riga][col + i] = 'N';
+                else p.scacchieraDifesa[riga + i][col] = 'N';
+            }
+            posizionata = true;
+        }
+    } while(!posizionata);
 }
 
-void iniziaGioco(int naviDa6 ,int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi, Giocatore ordinePlayer) {
-    int selezione, newPos = 6, oldPos = 6;
-    char comando;
-
-    for(int i = 0; i < 30; i++) {
-        for(int j = 0; j < 10; j++) {
-            ordinePlayer.scacchiera[i][j] = 'O';
+void inizializzaGiocatore(Giocatore &p) {
+    p.colpiASegno = 0;
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 30; j++) {
+            p.scacchieraDifesa[i][j] = 'O';
+            p.scacchieraAttacco[i][j] = 'O';
         }
     }
-    scacchiere();
-    gotoXY(66,3);
-    cout << "Inserire navi:";
-    gotoXY(66,6);
-    cout << "Navi da 6:  " << naviDa6;
-    gotoXY(66,8);
-    cout << "Navi da 5:  " << naviDa5;
-    gotoXY(66,10);
-    cout << "Navi da 4:  " << naviDa4;
-    gotoXY(66,12);
-    cout << "Navi da 3:  " << naviDa4;
-    gotoXY(66,14);
-    cout << "Navi da 2:  " << naviDa2;
-
-    do {
-        gotoXY(65,oldPos);
-        cout << " ";
-        gotoXY(65,newPos);
-        cout << ">";
-        oldPos = newPos;
-        oldPos = newPos;
-        comando = _getch();
-        if(comando == SU && newPos != 6) {
-            newPos -= 2;
-        }else if(comando == SU && newPos == 6) {
-            newPos = 14;
-        }
-        if(comando == GIU && newPos != 14) {
-            newPos += 2;
-        }else if(comando == GIU && newPos == 14){
-            newPos = 6;
-        }
-        if(comando == ENTER) {
-                selezione = newPos;
-                switch (selezione) {
-                    case 6:
-                        posizionamentoNavi(6);
-                        break;
-                }
-
-        }
-    }while (totNavi != 0);
-
-
-
 }
 
-void gestioneNavi(int nNavi6, int nNavi5, int nNavi4, int nNavi3, int nNavi2, int sommaNavi) {
+void iniziaGioco(int naviDa6 ,int naviDa5, int naviDa4, int naviDa3, int naviDa2, int totNavi) {
+    Giocatore p1, p2;
+    inizializzaGiocatore(p1);
+    inizializzaGiocatore(p2);
+
+    clearScreen(MAIN_BG, MAIN_FG);
+    gotoXY(2,2); cout << "Nome Giocatore 1: "; cin >> p1.nome;
+    gotoXY(2,4); cout << "Nome Giocatore 2: "; cin >> p2.nome;
+
+    // Posizionamento P1
+    clearScreen(MAIN_BG, MAIN_FG);
+    gotoXY(0,0); cout << "Posizionamento navi: " << p1.nome;
+    scacchiere();
+    // Cicli molto semplici per posizionare in sequenza
+    for(int i=0; i<naviDa6; i++) { gotoXY(0,18); cout << "Piazza nave da 6"; posizionamentoNavi(6, p1); }
+    for(int i=0; i<naviDa5; i++) { gotoXY(0,18); cout << "Piazza nave da 5"; posizionamentoNavi(5, p1); }
+    // (Per brevità non metto tutte le altre navi nel test testuale, ma la logica è identica)
+
+    // Posizionamento P2
+    clearScreen(MAIN_BG, MAIN_FG);
+    gotoXY(0,0); cout << "Passa il computer a " << p2.nome << " e premi un tasto..."; _getch();
+    clearScreen(MAIN_BG, MAIN_FG);
+    gotoXY(0,0); cout << "Posizionamento navi: " << p2.nome;
+    scacchiere();
+    for(int i=0; i<naviDa6; i++) { gotoXY(0,18); cout << "Piazza nave da 6"; posizionamentoNavi(6, p2); }
+    for(int i=0; i<naviDa5; i++) { gotoXY(0,18); cout << "Piazza nave da 5"; posizionamentoNavi(5, p2); }
+
+    // Avvia battaglia
+    faseBattaglia(p1, p2, totNavi);
+}
+
+void faseBattaglia(Giocatore &p1, Giocatore &p2, int flottaTotale) {
+    bool vittoria = false;
+    int turno = 1;
+    time_t tempoInizio = time(NULL);
+
+    while(!vittoria) {
+        Giocatore &attaccante = (turno % 2 != 0) ? p1 : p2;
+        Giocatore &difensore = (turno % 2 != 0) ? p2 : p1;
+
+        clearScreen(MAIN_BG, MAIN_FG);
+        gotoXY(0,0); cout << "TURNO DI ATTACCO: " << attaccante.nome;
+        scacchiere(); // Qui in realtà andrebbe stampata la scacchieraAttacco dell'attaccante
+
+        char rigaLettera;
+        int colNumero;
+
+        gotoXY(0,18); cout << "Inserisci coordinate (es. A 5): ";
+        cin >> rigaLettera >> colNumero;
+
+        // Conversione coordinate testo
+        int riga = toupper(rigaLettera) - 'A';
+        int col = colNumero - 1;
+
+        if(riga >= 0 && riga <= 9 && col >= 0 && col <= 29) {
+            if(difensore.scacchieraDifesa[riga][col] == 'N') {
+                gotoXY(0,20); cout << "COLPITA!";
+                difensore.scacchieraDifesa[riga][col] = 'X';
+                attaccante.colpiASegno++;
+            } else {
+                gotoXY(0,20); cout << "ACQUA!";
+            }
+            Sleep(1500);
+
+            if(attaccante.colpiASegno >= flottaTotale) { // Semplificazione: vince se colpisce tutti i pezzi
+                vittoria = true;
+                clearScreen(MAIN_BG, MAIN_FG);
+                gotoXY(10,10); cout << "HA VINTO " << attaccante.nome << "!";
+
+                // Salvataggio basico in classifica
+                if(numGiocatoriInClassifica < 10) {
+                    time_t tempoFine = time(NULL);
+                    strcpy(classificaTop[numGiocatoriInClassifica].nome, attaccante.nome);
+                    classificaTop[numGiocatoriInClassifica].flotta = flottaTotale;
+                    classificaTop[numGiocatoriInClassifica].tempoImpiegato = tempoFine - tempoInizio;
+                    numGiocatoriInClassifica++;
+                }
+                _getch();
+            } else {
+                turno++;
+            }
+        }
+    }
+}
+
+// La tua gestione navi è perfetta, ho solo aggiunto le '&' ai parametri nell'intestazione
+void gestioneNavi(int &nNavi6, int &nNavi5, int &nNavi4, int &nNavi3, int &nNavi2, int &sommaNavi) {
     int selezione, newPosX = 30, oldPosX = 30, newPosY = 6, oldPosY = 6;
     char comando;
 
-    cout << "	      BATTAGLIA NAVALE                Data : ";
-    displayToday();
-    gotoXY(0,1);
-    cout << "===============================================================" << endl;
-    gotoXY(7,3);
-    cout << "NAVI DISPONIBILI:    (Premere ESC per uscire)";
-    gotoXY(12,6);
-    cout << "Navi da 6 :        -   " << nNavi6 << "   +";
-    gotoXY(12,8);
-    cout << "Navi da 5 :        -   " << nNavi5 << "   +";
-    gotoXY(12,10);
-    cout << "Navi da 4 :        -   " << nNavi4 << "   +";
-    gotoXY(12,12);
-    cout << "Navi da 3 :        -   " << nNavi3 << "   +";
-    gotoXY(12,14);
-    cout << "Navi da 2 :        -   " << nNavi2 << "   +";
-
+    clearScreen(MAIN_BG, MAIN_FG);
+    gotoXY(0,1); cout << "===============================================================" << endl;
+    gotoXY(7,3); cout << "NAVI DISPONIBILI:    (Premere ESC per uscire)";
+    gotoXY(12,6); cout << "Navi da 6 :        -   " << nNavi6 << "   +";
+    gotoXY(12,8); cout << "Navi da 5 :        -   " << nNavi5 << "   +";
+    gotoXY(12,10); cout << "Navi da 4 :        -   " << nNavi4 << "   +";
+    gotoXY(12,12); cout << "Navi da 3 :        -   " << nNavi3 << "   +";
+    gotoXY(12,14); cout << "Navi da 2 :        -   " << nNavi2 << "   +";
+    gotoXY(0,17); cout << "===============================================================" << endl;
 
     gotoXY(0,17);
     cout << "===============================================================" << endl;
@@ -476,39 +454,43 @@ void gestioneNavi(int nNavi6, int nNavi5, int nNavi4, int nNavi3, int nNavi2, in
         }
     }while (comando != ESC || comando != ENTER);
 
-
-
 }
+
 
 void classifica() {
+    clearScreen(MAIN_BG, MAIN_FG);
+    gotoXY(5, 3); cout << "=== CLASSIFICA (Top 10 di questa sessione) ===";
+    gotoXY(5, 5); cout << "NOME\tFLOTTA\tTEMPO (sec)";
 
+    for(int i = 0; i < numGiocatoriInClassifica; i++) {
+        gotoXY(5, 7 + i);
+        cout << classificaTop[i].nome << "\t" << classificaTop[i].flotta << "\t" << classificaTop[i].tempoImpiegato;
+    }
+
+    if(numGiocatoriInClassifica == 0) {
+        gotoXY(5, 7); cout << "Ancora nessuna partita giocata.";
+    }
+
+    gotoXY(5, 19); cout << "Premi un tasto per tornare al menu...";
+    _getch();
 }
-
 
 void esci() {
-
+    clearScreen(0, 15);
+    gotoXY(10, 10);
+    cout << "Chiusura in corso...";
+    giocoInEsecuzione = false; // Ferma il loop nel main
 }
 
-
-/*
-    analisi tecnica : funzioni usate
-
-    gestione navi
-        numero navi predefinito
-        dimensioni predefinite
-        vuoi modificare
-        per ogni nave scegliere le dimensioni
-
-    controlloPosizionamentoNavi
-*/
-
-
 int main() {
-    int naviDa6 = 1, naviDa5 = 2, naviDa4 = 2, naviDa3 = 3, naviDa2 = 3, totNavi = 39;
+    int naviDa6 = 1, naviDa5 = 2, naviDa4 = 2, naviDa3 = 3, naviDa2 = 3;
+    int totNavi = (naviDa6*6) + (naviDa5*5) + (naviDa4*4) + (naviDa3*3) + (naviDa2*2);
+
     clearScreen(MAIN_BG, MAIN_FG);
     setColor(MAIN_BG, MAIN_FG);
-    do {
+
+    while(giocoInEsecuzione) {
         menu(naviDa6, naviDa5, naviDa4, naviDa3, naviDa2, totNavi);
-    } while(3 == 100);
+    }
     return 0;
 }
